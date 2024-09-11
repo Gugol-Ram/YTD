dc390b54a0cc8c57d2e294c9976321bd4ca4000c
Ramiro Heredia <94948260+Gugol-Ram@users.noreply.github.com>
2024-09-11 20:50:03 -0300

    ...

---
+++ musiquita.py
@@ -0,0 +1,26 @@
+# copiar enlace del video y ejecutar en la consola:
+
+# python musiquita.py
+
+import yt_dlp
+from pydub import AudioSegment
+import os
+
+# Enlace del video de YouTube
+url = 'https://www.youtube.com/watch?v=yNycw9zhO98&ab_channel=Monolink'
+
+# Configuración de yt-dlp para descargar solo el audio
+ydl_opts = {
+    'format': 'bestaudio/best',
+    'outtmpl': 'C:/Users/rami_/Downloads/Music Youtube/%(title)s.%(ext)s',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '192',
+    }],
+}
+
+with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+    ydl.download([url])
+
+print("La descarga y conversión se completaron exitosamente.")
